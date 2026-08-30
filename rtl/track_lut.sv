da3b1606fe44c76cbe9ad556cb29de59834d60bb
Jachymiak Tomasz <jimmi200503@gmail.com>
2026-08-30 19:35:49 +0200

    nowy track lut?

--- rtl/track_lut.sv
+++ rtl/track_lut.sv
@@ -1,18 +1,15 @@
-// AUTOMATYCZNIE WYGENEROWANY PLIK - NIE EDYTUJ RECZNIE
-// Skrypt wav_to_sd.py
-
+// AUTOMATYCZNIE WYGENEROWANY PLIK
 `timescale 1ns / 1ps
 
 module track_lut (
     input  logic [7:0]  track_id,
     output logic [31:0] start_lba
 );
-
     always_comb begin
         case (track_id)
-            8'd0: start_lba = 32'd0; // Plik: dj blik - balsam.wav
-            8'd1: start_lba = 32'd29532; // Plik: dj blik - h2o feat. dominika p#U0142onka.wav
-            8'd2: start_lba = 32'd48325; // Plik: dj blik - horyzont.wav
+            8'd0: start_lba = 32'd0;
+            8'd1: start_lba = 32'd29532;
+            8'd2: start_lba = 32'd48325;
             default: start_lba = 32'd0;
         endcase
     end
